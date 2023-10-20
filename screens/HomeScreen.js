bf10abbb65b91c2afd284d32a009f02d550655b8
rudrachouhan <rudra678chouhan@gmail.com>
2023-10-20 16:25:25 +0530

    Updated search functionality

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,44 +1,47 @@
-import { View, Text, ScrollView, Image, Pressable, TouchableOpacity, TextInput } from 'react-native'
-import React, { useEffect, useState,useCallback } from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
-import logo from '../assets/images/logo1.png'
-import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
-import Details from '../components/Details';
-import axios from 'axios';
-import { AntDesign } from '@expo/vector-icons';
-import { Ionicons } from '@expo/vector-icons';
+import {
+  View,
+  Text,
+  ScrollView,
+  Image,
+  Pressable,
+  TouchableOpacity,
+  TextInput,
+} from "react-native";
+import React, { useEffect, useState} from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import logo from "../assets/images/logo1.png";
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from "react-native-responsive-screen";
+import Details from "../components/Details";
+import axios from "axios";
+import { AntDesign } from "@expo/vector-icons";
+import { Ionicons } from "@expo/vector-icons";
 
 const HomeScreen = () => {
+
   const [arr, setArr] = useState([]);
   const [login, setLogin] = useState(false);
-  const [searchinput, setSearchinput] = useState('');
+  const [searchinput, setSearchinput] = useState("");
   const [show, setShow] = useState(true);
-  const [searchedUser, setSearchedUser] = useState({});
-  const [found, setFound] = useState(false);
+  const [results, setResults] = useState([]);
+  const [initialData, setInitialData] = useState([]);
+
   
 
   const goBack = () => {
     setShow(true);
-    setFound(false);
+    setResults(initialData);
+    setSearchinput("");
   };
 
   function handleArr(newValue) {
     setArr(newValue);
   }
 
-  const handleSearch = () => {
-    setShow(false);
-    arr.map((user) => {
-      if (user.user.name == searchinput) {
-        setSearchedUser(user);
-        setFound(true);
-      }
-    })
-  }
-
-  function handleShow(){
+  function handleShow() {
     setShow(true);
-    setFound(false);
   }
 
   async function handleLogin() {
@@ -52,8 +55,8 @@ const HomeScreen = () => {
 
   async function handleLogout() {
     try {
-      await axios.post('http://75.101.246.39:5000/logout')
-      setLogin(false)
+      await axios.post("http://75.101.246.39:5000/logout");
+      setLogin(false);
     } catch (error) {
       console.log(error);
     }
@@ -72,52 +75,118 @@ const HomeScreen = () => {
     fetch('http://75.101.246.39:5000/getUsers').then(data => {
       return data.json().then(data => {
         setArr(data);
+        setResults(data);
+        setInitialData(data);
       })
     }).catch(error => { console.log(error) })
-  }, [])
+  
+  }, []);
+
+  useEffect(() => {
+    if (results != undefined) {
+      const finalResults = results.filter((result) => {
+        return result.user.name.toLowerCase().indexOf(searchinput.toLowerCase()) !== -1;
+      });
+
+      setResults(finalResults);
+    }
+  }, [searchinput]);
 
   return (
     <SafeAreaView>
       <ScrollView>
-        <View className='flex flex-row items-center'>
-          <Pressable onPress={goBack} className='mr-16 mb-7 ml-4'>
+        <View className="flex flex-row items-center">
+          <Pressable onPress={goBack} className="mr-16 mb-7 ml-4">
             <Ionicons name="arrow-back" size={40} color="black" />
-            </Pressable>
-          <Image source={logo} style={{ width: wp(40) }} resizeMode='contain' className='mt-[-8%]' />
+          </Pressable>
+          <Image
+            source={logo}
+            style={{ width: wp(40) }}
+            resizeMode="contain"
+            className="mt-[-8%]"
+          />
         </View>
-        <View className='-mt-10 mx-auto px-4 border-[2px] border-gray-300 rounded-lg flex flex-row items-center' style={{width: wp(80)}} >
-          <TextInput className='p-2 rounded-lg' style={{ width: wp(65) }} value={searchinput} placeholder='Search a User' onChangeText={(e) => setSearchinput(e)}>
-          </TextInput>
-        <Pressable onPress={handleSearch}><AntDesign name="search1" size={24} color="black" /></Pressable>
+        <View
+          className="-mt-10 mx-auto px-4 border-[2px] border-gray-300 rounded-lg flex flex-row items-center"
+          style={{ width: wp(80) }}
+        >
+          <TextInput
+            className="p-2 rounded-lg"
+            style={{ width: wp(65) }}
+            value={searchinput}
+            placeholder="Search a User"
+            onChangeText={(e) => setSearchinput(e)}
+            onPressIn={() => setShow(false)}
+          ></TextInput>
+          <Pressable>
+            <AntDesign name="search1" size={24} color="black" />
+          </Pressable>
         </View>
-        <View className='mt-8 ml-3'>
-          <Text className='mt-[-5%] text-2xl ml-7 text-green-400 font-semibold'>Hi! 👋</Text>
-          <Text className='text-orange-500 mt-4 ml-7 text-xl tracking-wider font-semibold'>Delivery<Text className='text-green-400'>MATE</Text></Text>
+        <View className="mt-8 ml-3">
+          <Text className="mt-[-5%] text-2xl ml-7 text-green-400 font-semibold">
+            Hi! 👋
+          </Text>
+          <Text className="text-orange-500 mt-4 ml-7 text-xl tracking-wider font-semibold">
+            Delivery<Text className="text-green-400">MATE</Text>
+          </Text>
         </View>
-        {(login) ? <View className='mt-8'>
-          {show &&
-            arr.map((data) => {
-              return (
-                <Details key={data.userId} userId={data.userId} id={data.id} name={data.user.name} address={data.user.address} handleArr={handleArr} data={arr} handleShow={handleShow} />
-              )
-            })
-          }
-          {(!show) && <View>
-            {found ? <Details key={searchedUser.userId} userId={searchedUser.userId} id={searchedUser.id} name={searchedUser.user.name} address={searchedUser.user.address} handleArr={handleArr} data={arr} handleShow={handleShow} /> : <Text className='text-2xl ml-6'>No User Found</Text>}
-          </View>}
-          <View>
-            <TouchableOpacity onPress={handleLogout} className="mx-7 py-3 bg-orange-500 rounded-xl my-8" style={{ width: wp(85) }}>
-              <Text className='text-white text-3xl text-center'>Logout</Text>
-            </TouchableOpacity>
+        {login ? (
+          <View className="mt-8">
+            {show &&
+              arr.map((data) => {
+                return (
+                  <Details
+                    key={data.userId}
+                    userId={data.userId}
+                    id={data.id}
+                    name={data.user.name}
+                    address={data.user.address}
+                    handleArr={handleArr}
+                    data={arr}
+                    handleShow={handleShow}
+                  />
+                );
+              })}
+            {!show && (
+              <View>
+                {results.map((data) => {
+                  return (
+                    <Details
+                      key={data.userId}
+                      userId={data.userId}
+                      id={data.id}
+                      name={data.user.name}
+                      address={data.user.address}
+                      handleArr={handleArr}
+                      data={arr}
+                      handleShow={handleShow}
+                    />
+                  );
+                })}
+              </View>
+            )}
+            <View>
+              <TouchableOpacity
+                onPress={handleLogout}
+                className="mx-7 py-3 bg-orange-500 rounded-xl my-8"
+                style={{ width: wp(85) }}
+              >
+                <Text className="text-white text-3xl text-center">Logout</Text>
+              </TouchableOpacity>
+            </View>
           </View>
-        </View> :
-          <TouchableOpacity onPress={handleLogin} className="mx-7 py-3 bg-green-400 rounded-xl mt-5" style={{ width: wp(85) }}>
-            <Text className='text-white text-3xl text-center'>Login</Text>
+        ) : (
+          <TouchableOpacity
+            onPress={handleLogin}
+            className="mx-7 py-3 bg-green-400 rounded-xl mt-5"
+            style={{ width: wp(85) }}
+          >
+            <Text className="text-white text-3xl text-center">Login</Text>
           </TouchableOpacity>
-        }
+        )}
       </ScrollView>
     </SafeAreaView>
-  )
-}
+  );
+};
 
-export default HomeScreen
+export default HomeScreen;
